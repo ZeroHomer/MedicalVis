ec2a31f3409977650f6da12424a13b3cd23a5cac
ZeroHomer <56496833+ZeroHomer@users.noreply.github.com>
2021-12-24 22:08:50 +0800

    修改Bug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets
 import sys
-# from MyWidget import MyWindow
+#test
 from YKWMyWidget import MyWindow
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
